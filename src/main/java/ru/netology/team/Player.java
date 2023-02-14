277f4589fc5757b42f0ff95e2b00cba609f561c0
VasilevNik <heshelbe@inbox.ru>
2023-02-15 00:30:48 +0300

    Fix, bug fixed#2

--- src/main/java/ru/netology/team/Player.java
+++ src/main/java/ru/netology/team/Player.java
@@ -34,6 +34,7 @@ public class Player {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
             playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, hours);
         } else {
             playedTime.put(game, hours);
         }
